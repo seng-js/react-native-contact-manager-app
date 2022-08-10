5658c35675508d11f772b92bf23852c0b1fee579
Mov Seng <mov.seng@mindgeekconsultants.com>
2022-08-10 12:06:28 +0700

    Improve contact form

--- src/screens/FormScreen.js
+++ src/screens/FormScreen.js
@@ -1,13 +1,15 @@
-import {Button, Image, Keyboard, SafeAreaView, ScrollView, StyleSheet, Text, View,} from "react-native";
+import {Button, Dimensions, Image, Keyboard, SafeAreaView, ScrollView, StyleSheet, Text, View} from "react-native";
 import * as React from "react";
 import {useEffect, useState} from "react";
 import {DarkGray, grey} from "../utils/Styles";
 import Input from '../components/Form/Input';
 import {CheckBox} from "@rneui/themed";
-import SelectList from "react-native-dropdown-select-list/index";
 import Colors from "../utils/Colors";
-import {RadioButton, RadioGroup} from 'react-native-flexi-radio-button'
+import {RadioButton} from 'react-native-flexi-radio-button';
+import SelectDropdown from 'react-native-select-dropdown';
+import {FontAwesome, Ionicons, MaterialCommunityIcons} from "@expo/vector-icons";
 
+const {width} = Dimensions.get('window');
 const FormScreen = ({route, navigation}) => {
     console.log('FormScreen' + Math.random());
     const dataList = [
@@ -16,6 +18,7 @@ const FormScreen = ({route, navigation}) => {
         {key:'3', value: 'Maharashtra'},
         {key:'4', value: 'Goa'},
     ];
+    const defaultAvata = require('../../assets/images/img-default.jpg');
     const [selected, setSelected] = useState("");
     const [isFavorite, setIsFavorite] = useState(false);
     const [isContact, setIsContact] = useState(false);
@@ -73,7 +76,6 @@ const FormScreen = ({route, navigation}) => {
     const onSelect = (index, value) => {
         console.log(index, value);
     }
-
     const listAvata = [
         {avata: require('../../assets/images/img0.jpg')},
         {avata: require('../../assets/images/img1.jpg')},
@@ -82,6 +84,39 @@ const FormScreen = ({route, navigation}) => {
         {avata: require('../../assets/images/img4.jpg')},
         {avata: require('../../assets/images/img6.jpg')},
         {avata: require('../../assets/images/img7.jpg')},
+    ];
+
+    const countriesWithFlags = [
+        {title: 'Avata Default', image: require('../../assets/images/img0.jpg')},
+        {title: 'Avata F1', image: require('../../assets/images/img1.jpg')},
+        {title: 'Avata M1', image: require('../../assets/images/img2.jpg')},
+        {title: 'Avata M2', image: require('../../assets/images/img3.jpg')},
+        {title: 'Avata F2', image: require('../../assets/images/img4.jpg')},
+        {title: 'Avata F3', image: require('../../assets/images/img5.jpg')},
+        {title: 'Avata M3', image: require('../../assets/images/img6.jpg')},
+        {title: 'Avata F4', image: require('../../assets/images/img7.jpg')}
+    ];
+
+    const listPosition = [
+        {title: 'Select Position', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Web Designer', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'UI Designer', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Senior full stack engineer', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Software engineer frontend', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Senior Software Engineer', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Software engineer backend', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Android developer', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Project coordinator', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Mobile Software Engineer', image: require('../../assets/images/flag_ukraine.png')},
+    ];
+
+    const listCity = [
+        {title: 'Ukraine, Kyiv', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Ukraine, Kharkiv', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Ukraine, Odessa', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Ukraine, Dnipro', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Ukraine, Lviv', image: require('../../assets/images/flag_ukraine.png')},
+        {title: 'Cambodia, Phnom Penh', image: require('../../assets/images/flag_cambodia.png')}
     ];
 
     const renderListAvata = listAvata.map(({avata}, key) => {
@@ -103,20 +138,68 @@ const FormScreen = ({route, navigation}) => {
         setSelected(route.params.position);
     }, [route.params]);
 
+    const [selectedValue, setSelectedValue] = useState("Customer");
+    //state created for select box
+    const [isSelected, setSelection] = useState(false);
+    const countries = [
+        'Egypt',
+        'Canada',
+        'Australia',
+        'Ireland',
+        'Brazil',
+        'England',
+        'Dubai',
+        'France',
+        'Saudi Arabia',
+        'Argentina',
+        'India',
+    ];
     return (
         <SafeAreaView style={{flex: 1}}>
             <ScrollView style={{padding: 10}}>
                 <View style={{marginVertical: 20}}>
-                    <RadioGroup
-                        style={{flexGrow: 1, flexDirection: 'row'}}
-                        size={14}
-                        thickness={1}
-                        color='#9575b2'
-                        selectedIndex={1}
-                        onSelect = {(index, value) => onSelect(index, value)}
-                    >
-                        {renderListAvata}
-                    </RadioGroup>
+                    <SelectDropdown
+                        data={countriesWithFlags}
+                        // defaultValueByIndex={1}
+                        defaultValue={{
+                          title: 'England',
+                          image: defaultAvata,
+                        }}
+                        onSelect={(selectedItem, index) => {
+                            console.log(selectedItem, index);
+                        }}
+                        buttonStyle={styles.dropdown3BtnStyle}
+                        renderCustomizedButtonChild={(selectedItem, index) => {
+                            return (
+                                <View style={styles.dropdown3BtnChildStyle}>
+                                    {selectedItem ? (
+                                        <Image source={selectedItem.image} style={styles.dropdown3BtnImage} />
+                                    ) : (
+                                        <Image source={defaultAvata} style={styles.dropdown3BtnImage}  />
+                                    )}
+                                    <Text style={styles.dropdown3BtnTxt}>{selectedItem ? selectedItem.title : 'Select avata'}</Text>
+                                    <FontAwesome name="chevron-down" color={'#444'} size={16} />
+                                </View>
+                            );
+                        }}
+                        dropdownStyle={styles.dropdown3DropdownStyle}
+                        rowStyle={styles.dropdown3RowStyle}
+                        selectedRowStyle={styles.dropdown1SelectedRowStyle}
+                        renderCustomizedRowChild={(item, index) => {
+                            return (
+                                <View style={styles.dropdown3RowChildStyle}>
+                                    <Image source={item.image} style={styles.dropdownRowImage} borderRadius={20} />
+                                    <Text style={styles.dropdown3RowTxt}>{item.title}</Text>
+                                </View>
+                            );
+                        }}
+                        searchInputStyle={styles.dropdown3searchInputStyleStyle}
+                        searchPlaceHolder={'Search here'}
+                        searchPlaceHolderColor={'#F8F8F8'}
+                        renderSearchInputLeftIcon={() => {
+                            return <FontAwesome name={'search'} color={'#FFF'} size={18} />;
+                        }}
+                    />
                     <Input
                         onChangeText={text => handleOnchange(text, 'name')}
                         onFocus={() => handleError(null, 'name')}
@@ -135,16 +218,95 @@ const FormScreen = ({route, navigation}) => {
                         value={inputs?.company}
                         error={errors.company}
                     />
-                    <SelectList
-                        placeholder="Select Position"
-                        onSelect={() => setInputs({position: selected, ...inputs})}
-                        setSelected={setSelected}
-                        inputStyles={{color: 'grey'}}
-                        dropdownTextStyles={{color: grey}}
-                        data={dataList}
-                        search={false}
-                        boxStyles={{borderRadius:8, marginTop: 10, borderColor: grey}} //override default styles
-                    />
+                    <View style={styles.selectContainer}>
+                        <SelectDropdown
+                            data={listPosition}
+                            // defaultValueByIndex={1}
+                            // defaultValue={{
+                            //   title: 'England',
+                            //   image: require('./Images/England.jpg'),
+                            // }}
+                            onSelect={(selectedItem, index) => {
+                                console.log(selectedItem, index);
+                            }}
+                            buttonStyle={styles.dropdown3BtnStyle}
+                            renderCustomizedButtonChild={(selectedItem, index) => {
+                                return (
+                                    <View style={styles.dropdown3BtnChildStyle}>
+                                        {selectedItem ? (
+                                            <MaterialCommunityIcons name="medal-outline" color={'#444'} size={28} />
+                                        ) : (
+                                            <MaterialCommunityIcons name="medal-outline" color={'#444'} size={28} />
+                                        )}
+                                        <Text style={styles.dropdown3BtnTxt}>{selectedItem ? selectedItem.title : 'Select position'}</Text>
+                                        <FontAwesome name="chevron-down" color={'#444'} size={18} />
+                                    </View>
+                                );
+                            }}
+                            dropdownStyle={styles.dropdown3DropdownStyle}
+                            rowStyle={styles.dropdown3RowStyle}
+                            selectedRowStyle={styles.dropdown1SelectedRowStyle}
+                            renderCustomizedRowChild={(item, index) => {
+                                return (
+                                    <View style={styles.dropdown3RowChildStyle}>
+                                        <Text style={styles.dropdown3RowTxt}>{item.title}</Text>
+                                    </View>
+                                );
+                            }}
+                            search
+                            searchInputStyle={styles.dropdown3searchInputStyleStyle}
+                            searchPlaceHolder={'Search here'}
+                            searchPlaceHolderColor={'#F8F8F8'}
+                            renderSearchInputLeftIcon={() => {
+                                return <FontAwesome name={'search'} color={'#FFF'} size={18} />;
+                            }}
+                        />
+                    </View>
+                    <View style={styles.selectContainer}>
+                        <SelectDropdown
+                            data={listCity}
+                            // defaultValueByIndex={1}
+                            // defaultValue={{
+                            //   title: 'England',
+                            //   image: require('./Images/England.jpg'),
+                            // }}
+                            onSelect={(selectedItem, index) => {
+                                console.log(selectedItem, index);
+                            }}
+                            buttonStyle={styles.dropdown3BtnStyle}
+                            renderCustomizedButtonChild={(selectedItem, index) => {
+                                return (
+                                    <View style={styles.dropdown3BtnChildStyle}>
+                                        {selectedItem ? (
+                                            <Image source={selectedItem.image} style={styles.dropdown3BtnImage} />
+                                        ) : (
+                                            <Ionicons name="location-outline" color={'#444'} size={28} />
+                                        )}
+                                        <Text style={styles.dropdown3BtnTxt}>{selectedItem ? selectedItem.title : 'Select city'}</Text>
+                                        <FontAwesome name="chevron-down" color={'#444'} size={18} />
+                                    </View>
+                                );
+                            }}
+                            dropdownStyle={styles.dropdown3DropdownStyle}
+                            rowStyle={styles.dropdown3RowStyle}
+                            selectedRowStyle={styles.dropdown1SelectedRowStyle}
+                            renderCustomizedRowChild={(item, index) => {
+                                return (
+                                    <View style={styles.dropdown3RowChildStyle}>
+                                        <Image source={item.image} style={styles.dropdownRowImage} borderRadius={20} />
+                                        <Text style={styles.dropdown3RowTxt}>{item.title}</Text>
+                                    </View>
+                                );
+                            }}
+                            search
+                            searchInputStyle={styles.dropdown3searchInputStyleStyle}
+                            searchPlaceHolder={'Search here'}
+                            searchPlaceHolderColor={'#F8F8F8'}
+                            renderSearchInputLeftIcon={() => {
+                                return <FontAwesome name={'search'} color={'#FFF'} size={18} />;
+                            }}
+                        />
+                    </View>
                     {errors.position && (
                         <Text style={{marginTop: 7, color: Colors.red, fontSize: 12}}>
                             {errors.position}
@@ -258,5 +420,140 @@ const styles = StyleSheet.create({
     },
     checkbox: {
         backgroundColor: grey
-    }
+    },
+    inpt: {
+        borderWidth: 2,
+        borderColor: "#0b8ed2",
+        borderRadius: 4,
+        padding: 8,
+        marginBottom: 15
+    },
+    selectContainer: {
+        marginTop: 10
+    },
+    shadow: {
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 6},
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+        elevation: 10,
+    },
+    header: {
+        flexDirection: 'row',
+        width,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#F6F6F6',
+    },
+    headerTitle: {color: '#000', fontWeight: 'bold', fontSize: 16},
+    saveAreaViewContainer: {flex: 1, backgroundColor: '#FFF'},
+    viewContainer: {flex: 1, width, backgroundColor: '#FFF'},
+    scrollViewContainer: {
+        flexGrow: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: '10%',
+        paddingBottom: '20%',
+    },
+
+    dropdown1BtnStyle: {
+        width: '80%',
+        height: 50,
+        backgroundColor: '#FFF',
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: '#444',
+    },
+    dropdown1BtnTxtStyle: {color: '#444', textAlign: 'left'},
+    dropdown1DropdownStyle: {backgroundColor: '#EFEFEF'},
+    dropdown1RowStyle: {backgroundColor: '#EFEFEF', borderBottomColor: '#C5C5C5'},
+    dropdown1RowTxtStyle: {color: '#444', textAlign: 'left'},
+    dropdown1SelectedRowStyle: {backgroundColor: 'rgba(0,0,0,0.1)'},
+    dropdown1searchInputStyleStyle: {
+        backgroundColor: '#EFEFEF',
+        borderRadius: 8,
+        borderBottomWidth: 1,
+        borderBottomColor: '#444',
+    },
+
+    dropdown2BtnStyle: {
+        width: '80%',
+        height: 50,
+        backgroundColor: '#444',
+        borderRadius: 8,
+    },
+    dropdown2BtnTxtStyle: {
+        color: '#FFF',
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    dropdown2DropdownStyle: {
+        backgroundColor: '#444',
+        borderRadius: 12,
+    },
+    dropdown2RowStyle: {backgroundColor: '#444', borderBottomColor: '#C5C5C5'},
+    dropdown2RowTxtStyle: {
+        color: '#FFF',
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    dropdown2SelectedRowStyle: {backgroundColor: 'rgba(255,255,255,0.2)'},
+    dropdown2searchInputStyleStyle: {
+        backgroundColor: '#444',
+        borderBottomWidth: 1,
+        borderBottomColor: '#FFF',
+    },
+
+    dropdown3BtnStyle: {
+        width: '100%',
+        height: 50,
+        backgroundColor: '#FFF',
+        paddingHorizontal: 0,
+        borderWidth: 1,
+        borderRadius: 8,
+        borderColor: grey,
+    },
+    dropdown3BtnChildStyle: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 18,
+        borderRadius: 20
+    },
+    dropdown3BtnImage: {width: 30, height: 30, resizeMode: 'cover', borderRadius: 20},
+    dropdown3BtnTxt: {
+        color: '#444',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginHorizontal: 12,
+    },
+    dropdown3DropdownStyle: {backgroundColor: 'slategray'},
+    dropdown3RowStyle: {
+        backgroundColor: 'slategray',
+        borderBottomColor: '#444',
+        height: 50,
+    },
+    dropdown3RowChildStyle: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingHorizontal: 18,
+    },
+    dropdownRowImage: {width: 30, height: 30, resizeMode: 'cover'},
+    dropdown3RowTxt: {
+        color: '#F1F1F1',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginHorizontal: 12,
+    },
+    dropdown3searchInputStyleStyle: {
+        backgroundColor: 'slategray',
+        borderBottomWidth: 1,
+        borderBottomColor: '#FFF',
+    },
 });
